b66c859208c407b816b65871862dbc18d5d5b26a
zayn-shayq <85700442+zayn-shayq@users.noreply.github.com>
2021-12-03 19:28:00 +0500

    myFirstComit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Data_Structure;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Quiz 05");
+        System.out.println("Git Hub");
+
+    }
+}
