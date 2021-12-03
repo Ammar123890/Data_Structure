db118efca8a27ef44a7ee0882d5a86f8040f984e
Syed Muhammad Ammar <syedammar123890@gmail.com>
2021-12-03 19:55:43 +0500

    6

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("This is Quiz 05");
+        System.out.println("This is Quiz 07");
         System.out.println("Git Hub");
         System.out.println("This is my first change");
         System.out.println("Change by Ammar");
